a3d6c0443aca218aa6c1669b400c38ebead8de10
tmazurek <tmazurek@windowslive.com>
2022-06-10 12:27:12 +0200

    dodanie end pointa /dupa ktory przyjmuje wartosci name

--- src/main/java/com/example/lesson2/Lesson2Application.java
+++ src/main/java/com/example/lesson2/Lesson2Application.java
@@ -2,12 +2,21 @@ package com.example.lesson2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Lesson2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson2Application.class, args);
+
 	}
 
+	@GetMapping("/dupa")
+	public String dupa(@RequestParam(value = "name", defaultValue = "defaultValue") String name){
+	return "hello "  + name;
+}
 }
