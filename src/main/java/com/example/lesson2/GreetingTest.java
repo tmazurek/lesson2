9f1080e9a7e5d3e7915b5e468fb9ba69e5bae115
tom <tmazurek@outlook.com>
2022-06-11 08:32:23 +0200

    testowa zabawa

---
+++ src/main/java/com/example/lesson2/GreetingTest.java
@@ -0,0 +1,11 @@
+package com.example.lesson2;
+
+public class GreetingTest {
+    long id;
+    String greeting;
+
+    public GreetingTest(long id, String greeting){
+        this.greeting=greeting;
+        this.id=id;
+    }
+}
